bc45f6072692efdf722b546bd57712f51f1d7ae5
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-31 18:55:55 +0900

    Initial Commit

---
+++ ReMine_TEST/ReMine_TESTApp.swift
@@ -0,0 +1,15 @@
+//
+//  ReMine_TESTApp.swift
+//  ReMine_TEST
+//
+
+import SwiftUI
+
+@main
+struct ReMine_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
